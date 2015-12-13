9df4b7e8b88b582551dd43f9430cd9561b928c2c
admin <admin@YeMacBookPro.local>
2015-12-13 18:48:06 -0500

    Add more comments Remove the box around the legend as the original pic does not have box around the legend in plot4

--- plot4.R
+++ plot4.R
@@ -11,6 +11,7 @@ plot4 <- function(directory = "~/coursera/exploratory/ExData_Plotting1") {
   t$DateTime <- strptime(paste(t$Date, t$Time), "%d/%m/%Y %H:%M:%S")
   # Plot 4
   png(filename="plot4.png", width=480, height=480)
+  # Set two rows two columns
   par(mfrow=c(2,2))
   # plot1
   plot(t$DateTime, t$Global_active_power, type='l', xlab='Date Time', ylab='Global Active Power (kilowatts)')
@@ -20,9 +21,10 @@ plot4 <- function(directory = "~/coursera/exploratory/ExData_Plotting1") {
   plot(t$DateTime, t$Sub_metering_1, col = 'black', type='l', xlab='Date Time', ylab='Energy sub metering')
   lines(t$DateTime, t$Sub_metering_2, col = 'red', type='l')
   lines(t$DateTime, t$Sub_metering_3, col = 'blue', type='l')
-  legend('topright', legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col=c('black', 'red', 'blue'), lwd=1)
+  legend('topright', legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col=c('black', 'red', 'blue'), lwd=1, box.lty=0)
   # plot4
   plot(t$DateTime, t$Global_reactive_power, type='l', xlab='Date Time', ylab='Global Reactive Power (kilowatts)')
   dev.off()
+  # Set back to one row one column
   par(mfrow=c(1,1))
 }
